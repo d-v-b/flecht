9accc1ca57e2b9ed04fc3596406f82f2b1f344e6
Davis Vann Bennett <davis.v.bennett@gmail.com>
2019-09-02 17:20:37 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='flecht',
+   version='0.1.0',
+   description='Python-powered cellular automata',
+   author='Davis Bennett',
+   author_email='davis.v.bennett@gmail.com',
+   packages=['flecht'],
+   install_requires=['numpy', 'scipy'],
+)
