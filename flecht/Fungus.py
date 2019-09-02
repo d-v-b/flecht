15b33a1fa263353881e390cb7a328155e1df53f7
Davis Vann Bennett <davis.v.bennett@gmail.com>
2019-09-02 16:05:27 -0400

    renamed stencil to fungus

--- flecht/Fungus.py
+++ flecht/Fungus.py
@@ -1,17 +1,20 @@
 # Classes and methods for running cellular automata models
-
+from numpy import zeros, in1d, array, where
+from scipy.ndimage import correlate
+from itertools import combinations
+from operator import concat
+from functools import reduce
 
 class Fungus:
     """
     A fungus object for cellular automata simulations
     """
-    def __init__(self, filters, num_iter=1, trigger_state=1):
+    def __init__(self, filters, trigger_state=1):
         # ensure that filters is an iterable
         if not hasattr(filters, '__getitem__'):
-            raise TypeError('Filters must be an iterable, got ' + str(type(filters)) + ' instead.')
+            raise TypeError('Filters must be indexible, got ' + str(type(filters)) + ' instead.')
 
         self.filters = filters
-        self.num_iter = num_iter
         self._num_states = None
         self._trigger_state = trigger_state
 
@@ -25,19 +28,19 @@ class Fungus:
     def trigger_state(self):
         return self._trigger_state
 
-    def grow(self, seed_array, stability_check=None):
+    def grow(self, seed_array, num_iter=1, stability_check=None):
         """
         Iteratively apply filters to seed_array, returning a volume
         """
-        from numpy import zeros
 
         # initialize the array containing the result
         dims = list(seed_array.shape)
-        dims.insert(0, self.num_iter)
-        vol = zeros(dims).astype('int')
+        dims.insert(0, num_iter)
+        vol = zeros(dims, dtype='int')
         vol[0] = seed_array
 
-        for t in range(1, self.num_iter):
+        for t in range(1, num_iter):
+            # embarrassingly parallelizable over states
             for s in range(self.num_states):
                 vol[t] += self.advance(s, vol[t-1])
 
@@ -51,26 +54,22 @@ class Fungus:
         """
         advance the cellular automaton simulation by one frame for a given state
         """
-        from numpy import in1d
-        from scipy.ndimage import correlate
-
-        s = cur_state
 
         # the next state is the value we will increment to if a cell advances
-        next_state = (s + 1) % self.num_states
+        next_state = (cur_state + 1) % self.num_states
 
         # the trigger state is the value used to satisfy the filter conditions
-        trigger_state = (s + self.trigger_state) % self.num_states
-        s_mask = seed_array == s
+        trigger_state = (cur_state + self.trigger_state) % self.num_states
+        s_mask = seed_array == cur_state
         u_mask = (seed_array == trigger_state).astype('int')
 
-        kernel = self.filters[s].kernel
-        rule = self.filters[s].rule
-
-        conved = correlate(u_mask, kernel, mode='wrap') * s_mask
+        stencil = self.filters[cur_state].stencil
+        rule = self.filters[cur_state].rule
+        # todo: make the mode depend on the topology of the field
+        conved = correlate(u_mask, stencil, mode='wrap') * s_mask
         ix = in1d(conved.ravel(), rule).reshape(conved.shape)
 
-        s_mask = s_mask.astype('int') * s
+        s_mask = s_mask.astype('int') * cur_state
         s_mask[ix] = next_state
 
         return s_mask
@@ -85,26 +84,22 @@ class Fungus:
 
 class Filter(object):
     """
-    A combination of a kernel and a rule which together determine conditions for a state transition
+    A combination of a stencil and a rule which together determine conditions for a state transition
     """
-    def __init__(self, kernel, rule):
-        self.kernel = kernel
+    def __init__(self, stencil, rule):
+        self.stencil = stencil
         self.rule = rule
 
     def generate_domain(self):
         """
         Return the all the configurations that will trigger a state change for this filter object
         """
-        from numpy import zeros, array, where
-        from itertools import combinations
-        from operator import concat
-        from functools import reduce
 
         # Find all the possible ways for the rule to match the filter
-        kernel_inds = where(self.kernel.ravel() > 0)[0]
+        kernel_inds = where(self.stencil.ravel() > 0)[0]
         cases = list(list(combinations(kernel_inds, r)) for r in self.rule)
         domain_size = sum(map(len, cases))
-        domain = zeros((domain_size, *self.kernel.shape))
+        domain = zeros((domain_size, *self.stencil.shape))
 
         if len(cases) > 1:
             cases_flat = reduce(concat, cases)
@@ -116,8 +111,3 @@ class Filter(object):
 
         return domain
 
-
-class FilterForge(object):
-    """ Class for generating Filters """
-
-    pass
